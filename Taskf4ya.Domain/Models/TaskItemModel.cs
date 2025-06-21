f5637ba5abdabe17f17266aa0fc75913ae16532a
itsMattAmaral <mattvinicius.contato@gmail.com>
2025-06-20 23:29:49 -0300

    Add Task4ya.Application project and TaskItemModel class

---
+++ Taskf4ya.Domain/Models/TaskItemModel.cs
@@ -0,0 +1,15 @@
+using Taskf4ya.Domain.Enums;
+
+namespace Taskf4ya.Domain.Models;
+
+public class TaskItemModel
+{
+	public int Id { get; set; }
+	public required string Title { get; set; }
+	public string? Description { get; set; }
+	public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+	public DateTime UpdatedAt { get; set; } = DateTime.UtcNow;
+	public DateTime? DueDate { get; set; }
+	public TaskItemStatus Status { get; set; } = TaskItemStatus.Pending;
+	public TaskItemPriority Priority { get; set; } = TaskItemPriority.Medium;
+}
