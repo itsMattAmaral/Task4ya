8cc7df289e594d6639a4ee36d55e22b08603d69a
itsMattAmaral <mattvinicius.contato@gmail.com>
2025-06-27 16:21:30 -0300

    Add migration to create Boards and BoardTaskItems tables

---
+++ Task4ya.Infrastructure/Migrations/20250627180639_UpdateBoardModel.cs
@@ -0,0 +1,67 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace Task4ya.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateBoardModel : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Boards",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Name = table.Column<string>(type: "character varying(100)", maxLength: 100, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Boards", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "BoardTaskItems",
+                columns: table => new
+                {
+                    BoardId = table.Column<int>(type: "integer", nullable: false),
+                    TaskGroupId = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_BoardTaskItems", x => new { x.BoardId, x.TaskGroupId });
+                    table.ForeignKey(
+                        name: "FK_BoardTaskItems_Boards_BoardId",
+                        column: x => x.BoardId,
+                        principalTable: "Boards",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_BoardTaskItems_TaskItems_TaskGroupId",
+                        column: x => x.TaskGroupId,
+                        principalTable: "TaskItems",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_BoardTaskItems_TaskGroupId",
+                table: "BoardTaskItems",
+                column: "TaskGroupId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "BoardTaskItems");
+
+            migrationBuilder.DropTable(
+                name: "Boards");
+        }
+    }
+}
