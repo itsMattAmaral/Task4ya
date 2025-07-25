fcf7138780ac5d87c5ee6466388204beb5202a35
itsMattAmaral <mattvinicius.contato@gmail.com>
2025-07-25 17:20:45 -0300

    Add BoardId column to TaskItems for task management association

---
+++ Task4ya.Infrastructure/Migrations/20250701200920_UpdateModelChanges.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Task4ya.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateModelChanges : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "BoardId",
+                table: "TaskItems",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "BoardId",
+                table: "TaskItems");
+        }
+    }
+}
